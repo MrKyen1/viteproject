296a6bd943c6ea81a68d991b47dc65f2f8ccd7c4
ngodangkien <kien.ngo@mail.canon>
2026-02-27 16:13:10 +0700

    video 40

---
+++ src/components/learn/MyComponent.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function  MyComponent (){
+  return (
+    <div>Hello world My Component</div>
+  )
+}
---
+++ src/components/learn/SecondComponent.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function SecondComponent() {
+  return (
+    <div>SecondComponent</div>
+  )
+}
+function ThirdComponent(){
+    return(
+        <div>Third Components</div>
+    )
+}
+
+export{SecondComponent,ThirdComponent}
